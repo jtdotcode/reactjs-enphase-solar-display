1ee850125158758a008141fc75985f442192997e
John <john.gt82@gmail.com>
2022-07-06 17:08:25 +1000

    fix: get solar data script

---
+++ scripts/GetSolarData.ps1
@@ -0,0 +1,6 @@
+#this script gets the data from the from the enphase api
+$jsonConfig = Get-Content -Raw -Path config.json | ConvertFrom-Json
+
+$response = Invoke-RestMethod "https://api.enphaseenergy.com/api/v2/systems/2319758/summary?key=$($jsonConfig.apikey)&user_id=$($jsonConfig.username)" -Method 'GET' -Headers $headers
+$response | ConvertTo-Json | Set-Content ../solardata.json
+
