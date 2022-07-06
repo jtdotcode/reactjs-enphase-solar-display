6405abe61d388aca0f62d48317f7c009a3c8242d
John <john.gt82@gmail.com>
2022-07-06 17:10:29 +1000

    fix: typo

--- scripts/GetSolarData.ps1
+++ scripts/GetSolarData.ps1
@@ -1,4 +1,4 @@
-#this script gets the data from the from the enphase api
+#this script gets the data from the enphase api
 $jsonConfig = Get-Content -Raw -Path config.json | ConvertFrom-Json
 
 $response = Invoke-RestMethod "https://api.enphaseenergy.com/api/v2/systems/2319758/summary?key=$($jsonConfig.apikey)&user_id=$($jsonConfig.username)" -Method 'GET' -Headers $headers
